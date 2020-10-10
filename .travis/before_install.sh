6370f7fda55ccfbd382566b84b6913027734b1fe
Thomas Verwijst <thomas@verwijst.net>
2020-10-10 21:45:14 +0200

    Remove test/build files

--- .travis/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install Rust
-if [[ -z ${TRAVIS_RUST_VERSION+w} ]]; then
-  curl https://sh.rustup.rs -sSf | bash -s -- -y
-fi
--- .travis/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-
-perl -p -i -e \
-    "s/^snips-nlu-parsers-ffi-macros = .*\$/snips-nlu-parsers-ffi-macros = { path = \"..\/..\/ffi\/ffi-macros\" \}/g" \
-    python/ffi/Cargo.toml
